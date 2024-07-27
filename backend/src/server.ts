820d8f3ac5d22923f936de44e8ab0f04b9dabb67
iamsaintAdrian <adriankimani5@gmail.com>
2024-07-27 17:25:53 +0300

    Confiugure to use cloud hosting for images

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,27 +1,39 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes'
+import userRoutes from './routes/userRoutes';
 import productRoutes from './routes/productRoutes';
-import orderRoutes from './routes/orderRoutes'
-import reviewRoutes from './routes/reviewRoutes'
-import multer from 'multer'
+import orderRoutes from './routes/orderRoutes';
+import reviewRoutes from './routes/reviewRoutes';
+import multer from 'multer';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import path from 'path';
-import cors from 'cors'
+import cors from 'cors';
+import { v2 as cloudinary } from 'cloudinary';
+import { Request } from 'express';
 
 
 dotenv.config();
 
 const app = express();
 const PORT = 5000;
-app.use(cors())
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/'); 
+app.use(cors());
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: async (req: Request, file: Express.Multer.File) => {
+    return {
+      folder: 'uploads',
+      format: 'png', 
+      public_id: file.originalname,
+    };
   },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname)); 
-  }
 });
 
 const upload = multer({ storage: storage });
@@ -33,19 +45,17 @@ mongoose.connect(process.env.MONGO_URI!).then(() => console.log('MongoDB connect
   .catch(err => console.log(err));
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
-app.use('/api/orders', orderRoutes)
-app.use('/api/reviews', reviewRoutes)
+app.use('/api/orders', orderRoutes);
+app.use('/api/reviews', reviewRoutes);
 
 app.post('/upload', upload.single('file'), (req, res) => {
   if (!req.file) {
     return res.status(400).send('No file uploaded.');
   }
-  
-  const fileUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
+
+  const fileUrl = req.file.path;
   res.status(200).json({ url: fileUrl });
 });
-
-app.use('/uploads', express.static('uploads'));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
